c4114b02231d3b0fe2b6e49ab60fa5896fda15b3
William Trang <shadowskarmory17@gmail.com>
2020-04-03 15:16:53 -0700

    added a couple TODOs and fixed a part of forgotpass js

--- public/forgotpass/forgotpass.js
+++ public/forgotpass/forgotpass.js
@@ -5,6 +5,7 @@ let homeBtn = document.getElementById("homeBtn");
 let submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener("click", function(){
+    //TODO: ADD THIS WITH REAL FUNCTIONALITY WHEN QUYEN IS DONE STYLING
     document.getElementById("preReset").style.display = "none";
     document.getElementById("postReset").style.display = "block";
 });
