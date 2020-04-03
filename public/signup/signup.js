9d8081078dfe2ce86362ac3b1a23e4e4f9adeb43
William Trang <shadowskarmory17@gmail.com>
2020-04-02 23:17:05 -0700

    added sign up authentication

--- public/signup/signup.js
+++ public/signup/signup.js
@@ -2,7 +2,25 @@ import {firebaseConfig} from "../init.js"
 firebase.initializeApp(firebaseConfig);
 
 const AUTH = firebase.auth();
+const DATABASE = firebase.database();
 
 document.getElementById("signIn").addEventListener("click", function(){
     location.assign("../login/login.html");
+});
+
+document.getElementById("signUpBtn").addEventListener("click", e => {
+    if(document.getElementById("password").value == document.getElementById("confirmPass").value){
+        AUTH.createUserWithEmailAndPassword(document.getElementById("username").value, document.getElementById("password").value).then(() => {
+            DATABASE.ref("users/"+AUTH.currentUser.uid).set({
+				forename: document.getElementById("firstName").value,
+                surname: document.getElementById("lastName").value,
+                email: document.getElementById("username").value,
+			}).then(() => window.location.href = "../login/login.html");
+        }).catch(e => alert(e));
+    }
+    else {
+        alert("Passwords don't match!");
+        document.getElementById("password").value = "";
+        document.getElementById("confirmPass").value = "";
+    }
 });
