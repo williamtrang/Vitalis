01b51f441df45a2e141be06ae789cfc3f26a2664
William Trang <shadowskarmory17@gmail.com>
2020-04-03 18:02:13 -0700

    basic screenflow on contact page

--- public/contact/contact.js
+++ public/contact/contact.js
@@ -8,14 +8,35 @@ let addContactDiv = document.getElementById("addContact");
 let contactType = document.getElementById("contactType");
 let addContactInfo = document.getElementById("addContactInfo");
 let nextBtn = document.getElementById("nextBtn");
+let finishBtn = document.getElementById("finishBtn");
+let backToListBtn = document.getElementById("backToList");
+let backToTypeBtn = document.getElementById("backToType");
 
 contactBtn.addEventListener("click", function(){
     contactList.style.display = "none";
     addContactDiv.style.display = "inline-block";
+    contactType.style.display = "inline-block";
     addContactInfo.style.display = "none";
 });
 
 nextBtn.addEventListener("click", function(){
     addContactInfo.style.display = "inline-block";
     contactType.style.display = "none";
+});
+
+finishBtn.addEventListener("click", function(){
+    contactList.style.display = "inline-block";
+    addContactDiv.style.display = "none";
+});
+
+backToListBtn.addEventListener("click", function(){
+    contactList.style.display = "inline-block";
+    addContactDiv.style.display = "none";
+});
+
+backToTypeBtn.addEventListener("click", function(){
+    contactList.style.display = "none";
+    addContactDiv.style.display = "inline-block";
+    contactType.style.display = "inline-block";
+    addContactInfo.style.display = "none";
 });
