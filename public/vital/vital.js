9220bae4ae4323ad513f26a7aad13f77c0d78766
William Trang <shadowskarmory17@gmail.com>
2020-04-05 15:39:19 -0700

    basic vital functionality

--- public/vital/vital.js
+++ public/vital/vital.js
@@ -2,13 +2,44 @@ import {firebaseConfig} from "../init.js"
 firebase.initializeApp(firebaseConfig);
 
 const AUTH = firebase.auth();
+const addVitalBtn = document.getElementById("addVital");
+const backBtn = document.getElementById("backBtn");
+const finishBtn = document.getElementById("finishBtn");
+const addVitalsPage = document.getElementById("addVitalsPage");
+const mainPage = document.getElementById("mainPage");
 
+//TODO IF TIME: ADD ANIMATIONS AND NOT A COMPLETELY NEW SCREEN FOR ADDING STUFF
+//TODO: DISPLAY VITALS IN CALENDAR AND STUFF
+//TODO: CALENDAR BUTTON AND DIFF VIEW BTN FUNCTIONALITY
 //if user is not signed in, return to login page
 AUTH.onAuthStateChanged(function(user) {
     if(!user){
         location.assign("../login/login.html");
         return;
     }
+});
+
+addVitalBtn.addEventListener("click", function(){
+    mainPage.style.display = "none";
+    addVitalsPage.style.display = "inline-block";
+});
+
+backBtn.addEventListener("click", function(){
+    mainPage.style.display = "inline-block";
+    addVitalsPage.style.display = "none";
+
+    document.getElementById("pulseInput").value = "";
+    document.getElementById("sugarInput").value = "";
+});
+
+finishBtn.addEventListener("click", function(){
+    mainPage.style.display = "inline-block";
+    addVitalsPage.style.display = "none";
+
+    //TODO: PROCESS INFO AND STORE IN DB
+
+    document.getElementById("pulseInput").value = "";
+    document.getElementById("sugarInput").value = "";
 });
 
 //------------------------------------------------NAVBAR FUNCTIONALITY------------------------------------------------
