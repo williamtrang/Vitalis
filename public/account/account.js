b902be1f3cb6f5784ba91e0bb58455fdd7ea43ea
William Trang <shadowskarmory17@gmail.com>
2020-04-04 19:22:42 -0700

    account work

--- public/account/account.js
+++ public/account/account.js
@@ -2,7 +2,13 @@ import {firebaseConfig} from "../init.js"
 firebase.initializeApp(firebaseConfig);
 
 const AUTH = firebase.auth();
-let logoutBtn = document.getElementById("logoutBtn"); //btn to logout
+let logoutBtn = document.getElementById("logoutBtn");     //btn to logout
+let settingsBtn = document.getElementById("settingsBtn"); //btn to access settings
+
+//opens the settings menu when the btn is pressed
+settingsBtn.addEventListener("click", function(){
+    
+});
 
 //logs user out when logout btn is pressed
 logoutBtn.addEventListener("click", function(){
